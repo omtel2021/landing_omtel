e57d6e24c5733a7f350ef4a39a6db0eb45126540
marn88 <marn88@DESKTOP-TT2IEQA.localdomain>
2021-07-21 18:39:15 -0300

    coneccion a base de datos funcionando, mas trancicion de inagen de fondo

--- datos.php
+++ datos.php
@@ -1,35 +1,37 @@
 <?php 
 // recibe datos de formulario
 //include 'conect.php'; // incluye php de conexion a bd
-//("localhost","root","","","")
-$conexion = mysqli_connect("localhost","root","","","") or die("error conectando con el servidor");
+//("localhost","root","contraseña","nombre de base","puerto")
+$conexion = mysqli_connect("localhost","root","","landing_omtel","3306") or die("error conectando con el servidor");
 if(!$conexion){
 echo "Error al conectar a la base de datos";
 }
 
 
-$nombre = $_POST["Nombre"];
-$apellido= $_POST["Apellido"];
-$telefono = $_POST["Telefono"];
-$calles = $_POST["Calles"];
-$Entre_calle = $_POST["Entre_calles"];
-$ciudad_localidad = $_POST["ciudad"];
-$provincia=$_POST["Provincia"];
+$nombre = $_POST["nombre"];
+$apellido= $_POST["apellido"];
+$telefono = $_POST["telefono"];
+$calles = $_POST["calles"];
+$entre_calle = $_POST["entre_calles"];
+$ciudad_localidad = $_POST["ciudad_localidad"];
+$provincia = $_POST["provincia"];
+/*$fibra_check = $_POST["fibra_check"];
+$portabilidad_check = $_POST["portabilidad_check"];*/
 
 // envia datos a la base de datos
-
-$insert=  "INSERT INTO formulario(nombre,telefono,calles,numero_puerta,ciudad) 
-VALUES ('$nombre','$telefono','$calles','$numero_puerta','$ciudad') " ;
+//agregar los valore esn INSERT y en VALUES
+$insert=  "INSERT INTO landing(nombre,apellido,telefono,calles,entre_calles,ciudad_localidad,provincia) 
+VALUES ('$nombre','$apellido','$telefono','$calles','$entre_calle','$ciudad_localidad', '$provincia') " ;
 
 //ejecutar consulta
 
 $resultado= mysqli_query($conexion,$insert);
 
-/*if(!$resultado){
+if(!$resultado){
     echo "error en el registro";
 }
 else{
-    echo "lísto tu registro esta completo";
-}*/
+    echo " lísto tu registro esta completo ";
+}
 //cerrar conexion
 mysqli_close($conexion);
