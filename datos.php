b1cde1d661a73a82d80079f3b3e2b9405cf4ef12
marn88 <marn88@DESKTOP-TT2IEQA.localdomain>
2021-07-26 19:07:37 -0300

    conexion a base de datos sin registros duplicados

--- datos.php
+++ datos.php
@@ -2,10 +2,13 @@
 // recibe datos de formulario
 //include 'conect.php'; // incluye php de conexion a bd
 //("localhost","root","contraseña","nombre de base","puerto")
+
+
 $conexion = mysqli_connect("localhost","root","","landing_omtel","3306") or die("error conectando con el servidor");
 if(!$conexion){
 echo "Error al conectar a la base de datos";
 }
+
 
 if(isset($_POST["nombre"],$_POST["apellido"],$_POST["telefono"],$_POST["calle"],$_POST["entre_calle"],$_POST["ciudad_localidad"],$_POST["provincia"])){
 
@@ -19,11 +22,20 @@ $provincia = $_POST["provincia"];
 
 }
 
+$check=mysqli_query($conexion,"SELECT * from clients WHERE nombre='$nombre' and apellido='$apellido' and telefono='$telefono' ");
+$checkrows=mysqli_num_rows($check);
 
+if($checkrows >0){
+
+
+echo "el usuario ya se encuentra registrado";
+
+}
+else{
 
 // envia datos a la base de datos
 //agregar los valore esn INSERT y en VALUES
-$insert=  "INSERT INTO landing(nombre,apellido,telefono,calle,entre_calle,ciudad_localidad,provincia) 
+$insert= "INSERT INTO landing(nombre,apellido,telefono,calle,entre_calle,ciudad_localidad,provincia) 
 VALUES ('$nombre','$apellido','$telefono','$calle','$entre_calle','$ciudad_localidad','$provincia') " ;
 
 //ejecutar consulta
@@ -36,6 +48,14 @@ if(!$resultado){
 else{
     echo " lísto tu registro esta completo ";
 }
+
+
+}
+
+
+
+
+
 //cerrar conexion
 mysqli_close($conexion);
 
