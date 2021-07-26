9aab8989f2076b9bae726f4302cf6403d03100c5
marn88 <marn88@DESKTOP-TT2IEQA.localdomain>
2021-07-26 15:34:01 -0300

    validacion de datos js completa

--- validation.js
+++ validation.js
@@ -1,16 +1,18 @@
 function validate(){
-    var nombre,telefono,calles, numero_puerta,ciudad,expresion
+    var nombre,apellido,telefono,calle,entre_calle,ciudad_localidad,expresion
     nombre= document.getElementById ("nombre").value;
+    apellido= document.getElementById ("apellido").value;
     telefono= document.getElementById ("telefono").value;
-    calles= document.getElementById ("calles").value;
-    numero_puerta= document.getElementById ("numero_puerta").value;
-    ciudad= document.getElementById ("ciudad").value;
+    calle= document.getElementById ("calle").value;
+    entre_calle= document.getElementById ("entre_calle").value;
+    ciudad_localidad= document.getElementById ("ciudad_localidad").value;
+
     /*expresion= / \w+@\w\.+[a-z]/; (expresion regular para evaluear correo electronico)*/
-    if(nombre==="" || telefono==="" ||calles===""||numero_puerta===""||calles===""){
+    if(nombre==="" ||apellido==="" ||telefono==="" ||calle===""||entre_calle===""||ciudad_localidad===""){
         alert("debe completar todos los campos")  
 
     }/* puede agregarse el correo electronico hay que usar la expresion regulara comentada mas arriba*/
-    else if(nombre.length>20 || telefono.length>20 ||calles.length>20||numero_puerta.length>10){
+    else if(nombre.length>30||apellido.length>30 || telefono.length>20 ||calle.length>30||entre_calle.length>30||ciudad_localidad.length>30){
        
         alert ("los valores son demasiado largos")
        
